ab3d29ba3f82d887fbe5c4dbac66a2d3b17cf868
Nicusor97 <nicolaepicatureanu@gmail.com>
2020-01-30 22:06:14 +0200

    fix CCL, CC, CI, CLC, CLLC, LDC, LLDC, for text to speech directory

--- text_to_speech/synthesize_text.py
+++ text_to_speech/synthesize_text.py
@@ -7,65 +7,8 @@ Example usage:
     python synthesize_text.py --ssml "<speak>Hello there.</speak>"
 """
 
+from google.cloud import texttospeech
 import argparse
-
-
-# [START tts_synthesize_text]
-def synthesize_text(text):
-    """Synthesizes speech from the input string of text."""
-    from google.cloud import texttospeech
-    client = texttospeech.TextToSpeechClient()
-
-    input_text = texttospeech.types.SynthesisInput(text=text)
-
-    # Note: the voice can also be specified by name.
-    # Names of voices can be retrieved with client.list_voices().
-    voice = texttospeech.types.VoiceSelectionParams(
-        language_code='en-US',
-        ssml_gender=texttospeech.enums.SsmlVoiceGender.FEMALE)
-
-    audio_config = texttospeech.types.AudioConfig(
-        audio_encoding=texttospeech.enums.AudioEncoding.MP3)
-
-    response = client.synthesize_speech(input_text, voice, audio_config)
-
-    # The response's audio_content is binary.
-    with open('output.mp3', 'wb') as out:
-        out.write(response.audio_content)
-        print('Audio content written to file "output.mp3"')
-# [END tts_synthesize_text]
-
-
-# [START tts_synthesize_ssml]
-def synthesize_ssml(ssml):
-    """Synthesizes speech from the input string of ssml.
-
-    Note: ssml must be well-formed according to:
-        https://www.w3.org/TR/speech-synthesis/
-
-    Example: <speak>Hello there.</speak>
-    """
-    from google.cloud import texttospeech
-    client = texttospeech.TextToSpeechClient()
-
-    input_text = texttospeech.types.SynthesisInput(ssml=ssml)
-
-    # Note: the voice can also be specified by name.
-    # Names of voices can be retrieved with client.list_voices().
-    voice = texttospeech.types.VoiceSelectionParams(
-        language_code='en-US',
-        ssml_gender=texttospeech.enums.SsmlVoiceGender.FEMALE)
-
-    audio_config = texttospeech.types.AudioConfig(
-        audio_encoding=texttospeech.enums.AudioEncoding.MP3)
-
-    response = client.synthesize_speech(input_text, voice, audio_config)
-
-    # The response's audio_content is binary.
-    with open('output.mp3', 'wb') as out:
-        out.write(response.audio_content)
-        print('Audio content written to file "output.mp3"')
-# [END tts_synthesize_ssml]
 
 
 if __name__ == '__main__':
@@ -80,7 +23,46 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
+    client = texttospeech.TextToSpeechClient()
+    response = None
+
     if args.text:
-        synthesize_text(args.text)
+        """Synthesizes speech from the input string of text."""
+        input_text = texttospeech.types.SynthesisInput(text=text)
+
+        # Note: the voice can also be specified by name.
+        # Names of voices can be retrieved with client.list_voices().
+        voice = texttospeech.types.VoiceSelectionParams(
+            language_code='en-US',
+            ssml_gender=texttospeech.enums.SsmlVoiceGender.FEMALE)
+
+        audio_config = texttospeech.types.AudioConfig(
+            audio_encoding=texttospeech.enums.AudioEncoding.MP3)
+
+        response = client.synthesize_speech(input_text, voice, audio_config)
     else:
-        synthesize_ssml(args.ssml)
+        """Synthesizes speech from the input string of ssml.
+
+        Note: ssml must be well-formed according to:
+            https://www.w3.org/TR/speech-synthesis/
+
+        Example: <speak>Hello there.</speak>
+        """
+        input_text = texttospeech.types.SynthesisInput(ssml=ssml)
+
+        # Note: the voice can also be specified by name.
+        # Names of voices can be retrieved with client.list_voices().
+        voice = texttospeech.types.VoiceSelectionParams(
+            language_code='en-US',
+            ssml_gender=texttospeech.enums.SsmlVoiceGender.FEMALE)
+
+        audio_config = texttospeech.types.AudioConfig(
+            audio_encoding=texttospeech.enums.AudioEncoding.MP3)
+
+        response = client.synthesize_speech(input_text, voice, audio_config)
+
+    if response is not None:
+        # The response's audio_content is binary.
+        with open('output.mp3', 'wb') as out:
+            out.write(response.audio_content)
+            print('Audio content written to file "output.mp3"')
